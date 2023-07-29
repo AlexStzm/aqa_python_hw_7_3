c8a5bad2f1e6d0353f2847a16121b0f7a712a465
AlexStzm <svetozargrey@ya.ru>
2023-07-29 18:01:24 +0300

    второе изменение

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
 
 # Строка для создания первого коммита
+# Строка для создания второго коммита
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
