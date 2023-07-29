8e3c284043da1d00a81a753f41acdab60502940d
AlexStzm <svetozargrey@ya.ru>
2023-07-29 18:03:02 +0300

    Revert "второе изменение"
    This reverts commit c8a5bad2f1e6d0353f2847a16121b0f7a712a465.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # This is a sample Python script.
 
 # Строка для создания первого коммита
-# Строка для создания второго коммита
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
