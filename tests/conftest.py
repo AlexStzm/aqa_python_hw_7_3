5a5195222d4b7e16fb8f152e78ce448af6aef261
AlexStzm <svetozargrey@ya.ru>
2023-07-30 14:15:20 +0300

    В файле conftest.py фикстуры объединены в одну browser_manager

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,9 @@ import pytest
 from selene.support.shared import browser
 
 @pytest.fixture(scope="function", autouse=True)
-def size_browser():
+def browser_manager():
     browser.config.window_width = 1680
     browser.config.window_height = 1050
-
-@pytest.fixture(scope="function", autouse=True)
-def base_url_browser(size_browser):
     browser.config.base_url = 'https://chocodiamond.ru'
     yield
     browser.quit()
