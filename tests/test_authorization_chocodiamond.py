ce0e713cedf6e890e1f7df3e23e1ba9f1440d4e8
AlexStzm <svetozargrey@ya.ru>
2023-07-30 18:29:08 +0300

    Произведён рефакторинг в файле test_authorization_chocodiamond.py

--- tests/test_authorization_chocodiamond.py
+++ tests/test_authorization_chocodiamond.py
@@ -5,7 +5,7 @@ from selene import have
 def test_valid_login_and_password(browser_manager):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login #email').type('test-email@test.com').press_tab()
+    browser.element('#email').type('test-email@test.com').press_tab()
     browser.element('#password').type('password').press_enter()
 
     browser.element('.co-checkout-title').should(have.text('История заказов'))
@@ -14,7 +14,7 @@ def test_valid_login_and_password(browser_manager):
 def test_invalid_login_with_wrong_email(browser_manager):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login #email').type('testemail@test.com').press_tab()
+    browser.element('#email').type('testemail@test.com').press_tab()
     browser.element('#password').type('password').press_enter()
 
     browser.element('.co-notice--danger').should(have.exact_text('Сочетание логина и пароля не подходит'))
@@ -23,7 +23,7 @@ def test_invalid_login_with_wrong_email(browser_manager):
 def test_invalid_login_with_wrong_password(browser_manager):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login #email').type('test-email@test.com').press_tab()
+    browser.element('#email').type('test-email@test.com').press_tab()
     browser.element('#password').type('abracadabra').press_enter()
 
     browser.element('.co-notice--danger').should(have.exact_text('Сочетание логина и пароля не подходит'))
@@ -32,16 +32,16 @@ def test_invalid_login_with_wrong_password(browser_manager):
 def test_invalid_login_with_empty_email(browser_manager):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login #password').type('password').press_enter()
+    browser.element('#password').type('password').press_enter()
     time.sleep(3)
 
-    browser.element('.co-form-controls .co-button').should(have.exact_text('Войти'))
+    browser.element('.js-co-login-submit').should(have.exact_text('Войти'))
 
 
 def test_invalid_login_with_empty_password(browser_manager):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login #email').type('test-email@test.com').press_enter()
+    browser.element('#email').type('test-email@test.com').press_enter()
     time.sleep(3)
 
-    browser.element('.co-form-controls .co-button').should(have.exact_text('Войти'))
+    browser.element('.js-co-login-submit').should(have.exact_text('Войти'))
