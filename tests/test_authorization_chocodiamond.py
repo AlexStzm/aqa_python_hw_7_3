0d07f384ed8da1004146a0478afefd058387f961
AlexStzm <svetozargrey@ya.ru>
2023-07-30 14:06:19 +0300

    Скорректирован код в test_authorization_chocodiamond.py

--- tests/test_authorization_chocodiamond.py
+++ tests/test_authorization_chocodiamond.py
@@ -4,8 +4,8 @@ from selene import have
 def test_valid_login_and_password(base_url_browser):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login [name=email]').type('test-email@test.com').press_tab()
-    browser.element('[name=password]').type('password').press_enter()
+    browser.element('.co-form--login #email').type('test-email@test.com').press_tab()
+    browser.element('#password').type('password').press_enter()
 
     browser.element('.co-checkout-title').should(have.text('История заказов'))
 
@@ -13,8 +13,8 @@ def test_valid_login_and_password(base_url_browser):
 def test_invalid_login_with_wrong_email(base_url_browser):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login [name=email]').type('testemail@test.com').press_tab()
-    browser.element('[name=password]').type('password').press_enter()
+    browser.element('.co-form--login #email').type('testemail@test.com').press_tab()
+    browser.element('#password').type('password').press_enter()
 
     browser.element('.co-notice--danger').should(have.exact_text('Сочетание логина и пароля не подходит'))
 
@@ -22,8 +22,8 @@ def test_invalid_login_with_wrong_email(base_url_browser):
 def test_invalid_login_with_wrong_password(base_url_browser):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login [name=email]').type('test-email@test.com').press_tab()
-    browser.element('[name=password]').type('abracadabra').press_enter()
+    browser.element('.co-form--login #email').type('test-email@test.com').press_tab()
+    browser.element('#password').type('abracadabra').press_enter()
 
     browser.element('.co-notice--danger').should(have.exact_text('Сочетание логина и пароля не подходит'))
 
@@ -31,7 +31,7 @@ def test_invalid_login_with_wrong_password(base_url_browser):
 def test_invalid_login_with_empty_email(base_url_browser):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login [name=password]').type('password').press_enter()
+    browser.element('.co-form--login #password').type('password').press_enter()
 
     browser.element('.co-form-controls .co-button').should(have.exact_text('Войти'))
 
@@ -39,6 +39,6 @@ def test_invalid_login_with_empty_email(base_url_browser):
 def test_invalid_login_with_empty_password(base_url_browser):
     browser.open('/client_account/session/new')
 
-    browser.element('.co-form--login [name=email]').type('test-email@test.com').press_enter()
+    browser.element('.co-form--login #email').type('test-email@test.com').press_enter()
 
     browser.element('.co-form-controls .co-button').should(have.exact_text('Войти'))
